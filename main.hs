23fc388905d15211edf9d48f213537f54a4e9b07
ElectricSolstice <repairagent@yahoo.com>
2016-07-01 21:48:48 -0700

    Updated for field changes

--- main.hs
+++ main.hs
@@ -1,14 +1,23 @@
 module Main (Main.main) where
-
+import Control.Monad (sequence_)
 import Data.Maybe (fromJust)
+import Data.List (elemIndex)
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.General.Enums
 
-data Book = Book { bookID :: String,
-    title :: String,
-    description :: String } deriving (Show)
+fields = ["BookID","Title", "Description"]
+textFields = drop 1 fields
+
+modelFields = 
+    foldr1 (++) ((map (++ " TEXT, ") (take ((length textFields)-1) textFields)) 
+        ++ (map (++ " TEXT ") (drop ((length textFields)-1) textFields)))
+
+getField field = elemIndex field fields
+
+createRowFilter :: Int -> ([String] -> [AttrOp CellRendererText])
+createRowFilter index = (\row -> [cellText := row !! index])
 
 addColumn :: (TreeViewClass view, TreeModelClass (model row), CellRendererClass renderer, 
     TypedTreeModelClass model) 
@@ -22,33 +31,37 @@ addColumn view renderer model name func = do
     return ()
 
 populateModel model connection = do
-    create <- prepare connection ("CREATE TABLE IF NOT EXISTS Books (BookID INTEGER PRIMARY KEY,"
-        ++ "Title TEXT, Description TEXT);")
+    create <- prepare connection ("CREATE TABLE IF NOT EXISTS Books (BookID " 
+        ++ "INTEGER PRIMARY KEY, " ++ modelFields ++ ")")
     execute create []
     commit connection
 
 updateModel model connection = do
     listStoreClear model
     allBooks model connection
+    return ()
 
 allBooks model connection = do
     select <- prepare connection "SELECT * FROM Books;"
     execute select []
     books <- fetchAllRows select
-    mapM_ (listStoreAppend model) 
-        (map (\row -> Book (row !! 0) (row !! 1) (row !! 2)) (map (map fromSql) books))
+    mapM_ (listStoreAppend model) (map (map fromSql) books)
     return books
 
-insertBook :: IConnection conn => conn -> Book -> IO ()
+insertBook :: IConnection conn => conn -> [String] -> IO ()
 insertBook connection book = do
-    insert <- prepare connection "INSERT INTO Books (Title, Description) Values (?,?)"
-    execute insert (map toSql [title book, description book])
+    sqlFields <- return ((map (++ ",") (take ((length textFields)-1) textFields))++(drop ((length textFields)-1) textFields))
+    sqlCols <- return (foldr (++) "" sqlFields)
+    sqlVals <- return ((foldr (++) "" (take ((length sqlFields)-1) (repeat "?,")))++"?")
+    insert <- prepare connection ("INSERT INTO Books ("++sqlCols
+        ++") Values ("++sqlVals++")")
+    execute insert (map toSql book)
     commit connection
+    return ()
 
 addBook model connection entries = do
-    title <- entryGetText (snd (head entries))
-    desc <- entryGetText (snd (head (tail entries)))
-    insertBook connection (Book "0" title desc)
+    book <- mapM (\x -> entryGetText (snd x)) entries
+    insertBook connection book
     updateModel model connection
     return ()
 
@@ -71,16 +84,16 @@ delBook model view connection = do
     selection <- treeViewGetSelection view
     selected <- treeSelectionGetSelected selection
     book <- listStoreGetValue model (listStoreIterToIndex (fromJust selected))
-    removeBook connection (bookID book)
+    removeBook connection (head book)
     updateModel model connection
     return ()
 
 addBookPopup model connection = do
     popup <- windowNew
     vbox <- vBoxNew False 0
-    fields <- mapM (addEntry vbox) ["Title","Description"]
+    entryFields <- mapM (addEntry vbox) textFields
     button <- buttonNewWithLabel "Add Book"
-    on button buttonActivated (addBook model connection fields)
+    on button buttonActivated (addBook model connection entryFields)
     boxPackStart vbox button PackNatural 0
     button <- buttonNewWithLabel "Cancel"
     on button buttonActivated (widgetDestroy popup)
@@ -96,7 +109,7 @@ main = do
     bookList <- scrolledWindowNew (Nothing) (Nothing)
     scrolledWindowSetPolicy bookList PolicyNever PolicyAutomatic
     on window objectDestroy mainQuit
-    model <- listStoreNew []
+    model <- listStoreNew ([] :: [[String]])
     view <- treeViewNewWithModel model
     vbox <- vBoxNew False 0
 
@@ -106,14 +119,11 @@ main = do
 
     renderer <- cellRendererTextNew
 
-    column <- treeViewColumnNew
-    treeViewColumnSetVisible column False
-    treeViewColumnPackStart column renderer False
-    cellLayoutSetAttributes column renderer model (\row -> [cellText := bookID row])
-    treeViewAppendColumn view column
+    columns <- return (map 
+        (addColumn view renderer model) textFields)
+    filters <- return (map createRowFilter [1..(length fields)-1])
+    sequence_ (zipWith (\ c f -> c f) (columns) (filters))
 
-    addColumn view renderer model "Title" (\row -> [cellText := title row])
-    addColumn view renderer model "Description" (\row -> [cellText := description row])
     containerAdd bookList view
     containerAdd vbox bookList
 
@@ -130,3 +140,4 @@ main = do
     widgetShowAll window
     mainGUI
     Database.HDBC.disconnect c
+
