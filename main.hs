58e7abe6fa1882e55c12493cca692958a39c4d60
ElectricSolstice <repairagent@yahoo.com>
2017-03-30 22:05:03 -0700

    Updated to not need FlexibleContexts. Also, column indexing now based on column ordering in sqlite file

--- main.hs
+++ main.hs
@@ -2,6 +2,7 @@ module Main (Main.main) where
 import Control.Monad (sequence_)
 import qualified Data.ByteString.Char8 (concat)
 import qualified Data.Text as Text (pack,unpack,split,strip) 
+import Data.Convertible.Base
 import Data.Maybe (fromJust)
 import Data.List (elemIndex)
 import Database.HDBC
@@ -18,10 +19,13 @@ modelFields =
     foldr1 (++) ((map (++ " TEXT, ") (take ((length textFields)-1) textFields)) 
         ++ (map (++ " TEXT ") (drop ((length textFields)-1) textFields)))
 
-getField field = elemIndex field fields
+getField :: IConnection connection => connection -> String -> String -> IO(Maybe Int)
+getField connection tableName field = do
+   tableCols <- getTableCols connection tableName
+   return (elemIndex field tableCols)
 
-createRowFilter :: Int -> ([String] -> [AttrOp CellRendererText])
-createRowFilter index = (\row -> [cellText := row !! index])
+createRowFilter :: Int -> ([SqlValue] -> [AttrOp CellRendererText])
+createRowFilter index = (\row -> [cellText := (show (extractByteString (row !! index)))])
 
 addColumn :: (TreeViewClass view, TreeModelClass (model row), CellRendererClass renderer, 
     TypedTreeModelClass model) 
@@ -34,19 +38,26 @@ addColumn view renderer model name func = do
     treeViewAppendColumn view column
     return ()
 
-populateModel model connection = do
-    create <- prepare connection ("CREATE TABLE IF NOT EXISTS Books (BookID " 
-        ++ "INTEGER PRIMARY KEY, " ++ modelFields ++ ")")
-    execute create []
-    getSchema <- prepare connection ("SELECT sql FROM sqlite_master WHERE type='table' AND name='Books';")
+getTableCols connection tableName = do
+    getSchema <- prepare connection ("SELECT sql FROM sqlite_master WHERE type='table' AND name='"++tableName++"';")
     execute getSchema []
     cols <- fetchRow getSchema
     sqlString <- return (map extractByteString (fromJust cols))
     str <- return (Data.ByteString.Char8.concat sqlString)
     tableVars <- return (takeWhile (/= ')') (drop 1 (dropWhile (/= '(') (show str))))
     tableCols <- return (map (Text.split (== ' ')) (map Text.strip (Text.split (== ',') (Text.pack(tableVars)))))
-    columns <- return (map (\a -> a !! 0) (map (map Text.unpack) tableCols))
-    missingColumns <- return (filter (\elem -> notElem elem columns) fields)
+    return (map (\a -> a !! 0) (map (map Text.unpack) tableCols))
+
+getMissingColumns connection tableName = do
+    columns <- getTableCols connection tableName
+    -- columns <- return (map (\a -> a !! 0) (map (map Text.unpack) tableCols))
+    return (filter (\elem -> notElem elem columns) fields)
+
+populateModel model connection = do
+    create <- prepare connection ("CREATE TABLE IF NOT EXISTS Books (BookID " 
+        ++ "INTEGER PRIMARY KEY, " ++ modelFields ++ ")")
+    execute create []
+    missingColumns <- getMissingColumns connection "Books"
     alterStatements <- return $ map (\m -> "ALTER TABLE Books ADD COLUMN " ++ m ++ " DEFAULT ''") missingColumns
     alters <- (mapM (prepare connection) alterStatements)
     mapM_ (\alt -> execute alt [])  alters
@@ -57,6 +68,7 @@ updateModel model connection = do
     allBooks model connection
     return ()
 
+allBooks :: IConnection conn => ListStore [SqlValue] -> conn -> IO [[SqlValue]]
 allBooks model connection = do
     select <- prepare connection "SELECT * FROM Books;"
     execute select []
@@ -91,6 +103,7 @@ addEntry box fieldName = do
     boxPackStart box hbox PackNatural 0
     return (fieldName, entry)
 
+removeBook :: IConnection conn => conn -> SqlValue -> IO ()
 removeBook connection bookKey = do
     del <- prepare connection "DELETE FROM Books WHERE BookID=?"
     execute del (map toSql [bookKey])
@@ -125,19 +138,21 @@ main = do
     bookList <- scrolledWindowNew (Nothing) (Nothing)
     scrolledWindowSetPolicy bookList PolicyNever PolicyAutomatic
     on window objectDestroy mainQuit
-    model <- listStoreNew ([] :: [[String]])
+    model <- listStoreNew ([] :: [[SqlValue]])
     view <- treeViewNewWithModel model
     vbox <- vBoxNew False 0
 
-    c <- connectSqlite3 "books.sqlite"
-    populateModel model c
-    allBooks model c
+    connection <- connectSqlite3 "books.sqlite"
+    populateModel model connection
+    allBooks model connection
 
     renderer <- cellRendererTextNew
 
     columns <- return (map 
         (addColumn view renderer model) textFields)
-    filters <- return (map createRowFilter [1..(length fields)-1])
+    --fieldIndexes <- map (\ t -> fromJust (getField connection model t)) textFields
+    fieldIndexes <- sequence (map (getField connection "Books") textFields)
+    filters <- return (map createRowFilter (map fromJust fieldIndexes))
     sequence_ (zipWith (\ c f -> c f) (columns) (filters))
 
     containerAdd bookList view
@@ -145,15 +160,15 @@ main = do
 
     buttonRow <- hBoxNew False 0
     button <- buttonNewWithLabel "Add Book"
-    on button buttonActivated (addBookPopup model c)
+    on button buttonActivated (addBookPopup model connection)
     boxPackStart buttonRow button PackNatural 0
     button <- buttonNewWithLabel "Delete Book"
-    on button buttonActivated (delBook model view c)
+    on button buttonActivated (delBook model view connection)
     boxPackStart buttonRow button PackNatural 0
     boxPackStart vbox buttonRow PackNatural 0
 
     containerAdd window vbox
     widgetShowAll window
     mainGUI
-    Database.HDBC.disconnect c
+    Database.HDBC.disconnect connection
 
